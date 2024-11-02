e2e81f1b72585ba9bcce5d517c4b734f7413856f
Guru20154 <gurkanwal04singh@gmail.com>
2024-11-02 17:36:50 +0530

    Basic routes and controllers

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import dotenv from "dotenv"; 
 import connectDB from "./db/index.js";
+import {app} from './app.js';
 
 dotenv.config({
     path: './env'
@@ -11,7 +12,7 @@ connectDB()
         console.log("MONGO ERROR: ", error)
         throw error
     })
-    app.listen(process.env.PORT || 8000,()=>{
+    app.listen(process.env.PORT || 4000,()=>{
         console.log(`Listening on port: ${process.env.PORT}`)
     })
 })
